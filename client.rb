6a0509efb54c0211ebeb86a1ee793faa93c53356
Martin Linkhorst <m.linkhorst@googlemail.com>
2010-10-26 01:47:40 +0200

    more sweetness

--- client.rb
+++ client.rb
@@ -1,4 +1,5 @@
 require 'socket'
+require 'singleton'
 
 module TWP3
   MAGIC_BYTES    = "TWP3\n"
@@ -33,8 +34,31 @@ module TWP3
   end
   
   class Connection
-    def initialize(host, port = 80)
+    include Singleton
+    
+    def initialize
+      @connected = false
+      at_exit { disconnect }
+    end
+    
+    def self.setup(host, port = 80)
+      instance.setup(host, port)
+    end
+    
+    def self.disconnect
+      instance.disconnect
+    end
+    
+    def self.connected?
+      instance.connected?
+    end
+    
+    def setup(host, port = 80)
       @host, @port = host, port
+    end
+    
+    def connected?
+      @connected
     end
     
     def protocol=(id)
@@ -46,14 +70,16 @@ module TWP3
         @socket = TCPSocket.new(@host, @port)
         @socket.write MAGIC_BYTES
         @socket.write @protocol
+        @connected = true
       rescue Exception
-        @socket.close
+        @socket.close if @socket && !@socket.closed?
         exit(1)
       end
     end
     
     def disconnect
-      @socket.close
+      @socket.close unless @socket.closed?
+      @connected = false
     end
     
     def read(*args)
@@ -89,6 +115,8 @@ module TWP3
     end
   
     class Response
+      attr_reader :message, :parameters
+      
       def initialize(connection)
         @connection = connection
         @parameters = []
@@ -128,31 +156,92 @@ module TWP3
     
     def initialize(&block)
       @protocols = []
-      instance_exec(&block)
+      instance_exec(&block) if block_given?
     end
+    
+    def method_missing(name, *args, &block)
+      @protocols.select {|p| p.name == name.to_s}.first || super
+    end      
   end
   
   class Protocol
-    attr_reader :messages
+    attr_reader :id, :name, :messages
     
     def initialize(name, id, &block)
       @name, @id = name, id
       @messages  = []
-      instance_exec(&block)
+      instance_exec(&block) if block_given?
     end
+
+    def method_missing(name, *args, &block)
+      method = @messages.select {|p| p.name == name.to_s}.first
+      method ? method.invoke(*args, &block) : super
+    end      
   end
   
   class Message
-    attr_reader :parameters
+    attr_reader :id, :name, :parameters
+    attr_writer :protocol
     
-    def initialize(name, id, &block)
-      @name, @id = name, id
+    def initialize(name, id, protocol, &block)
+      @name, @id, @protocol = name, id, protocol
       @parameters = []
-      instance_exec(&block)
+      instance_exec(&block) if block_given?
+    end
+    
+    def invoke(*args, &block)
+      # don't open a new connection for every message
+      #
+      # check wether con is already connected?
+      # if no: connect
+      # if yes: check protocol
+      #   if matches: do nothing
+      #   if not: disconnect and reconnect with new protocol
+      #
+      # need to close socket when program ends or class is killed
+      #
+      con = Connection.instance
+      if con.connected?
+        unless con.protocol == @protocol.id
+          con.disconnect
+          con.protocol = @protocol.id
+          con.connect          
+        end
+      else
+        con.protocol = @protocol.id
+        con.connect
+      end
+      
+      msg = Messages::Request.new(con)
+      msg.message = @id
+
+      args.each_with_index do |value, index|
+        case @parameters[index].type
+          when :int
+            then msg.parameters << Types::ShortInteger.new(args[index])
+          when :string
+            then msg.parameters << Types::ShortString.new(args[index])
+        end
+      end
+
+      msg.submit
+      
+      response = TWP3::Messages::Response.new(con)
+      response.receive
+      
+      msg = @protocol.messages.select{|m| m.id == response.message}.first
+      msg.parameters.each_index do |index|
+        msg.parameters[index].value = response.parameters[index]
+      end
+      
+      msg
     end
   end
   
   class Parameter
+    attr_reader :name, :type, :value
+    attr_writer :value
+    
     def initialize(name, type)
       @name, @type = name, type
     end
@@ -161,7 +250,7 @@ module TWP3
   # here's the magic
   
   module DSL
-    def self.included(base)
+    def self.extended(base)
       # add the following DSL methods to the classes only when we include the DSL module
       Specification.send :include, SpecificationMethods
       Protocol.send      :include, ProtocolMethods
@@ -169,7 +258,11 @@ module TWP3
     end
     
     def specification(&block)
-      Specification.new(&block)
+      @specification = Specification.new(&block)
+      @specification.protocols.each do |protocol|
+        (self.class == Class ? self : self.class).
+          const_set(protocol.name, @specification.protocols.first)
+      end
     end
     
     module SpecificationMethods
@@ -180,7 +273,7 @@ module TWP3
     
     module ProtocolMethods
       def message(name, id, &block)
-        @messages << Message.new(name, id, &block)
+        @messages << Message.new(name, id, self, &block)
       end
     end
     
@@ -200,16 +293,14 @@ module TWP3
   end
 end
 
-# only to encapsulate
-include TWP3::DSL
+# to encapsulate, can be wrapped by any object (e.g. TWPClient::Echo.Request)
+extend TWP3::DSL
 
-# sweet?!
-spec =
+# sweet, he?
 specification do
   protocol 'Echo', 2 do
     message 'Request', 0 do
       string :text
-      # short for parameter :text, :string
     end
     message 'Reply', 1 do
       string :text
@@ -220,46 +311,67 @@ end
 
 # same as:
 
-# message1 = TWP3::Message.new('Request', 0)
+# spec = TWP3::Specification.new
+# protocol = TWP3::Protocol.new('Echo', 2)
+# 
+# message1 = TWP3::Message.new('Request', 0, protocol)
 # message1.parameters << TWP3::Parameter.new('text', :string)
 # 
-# message2 = TWP3::Message.new('Reply', 1)
+# message2 = TWP3::Message.new('Reply', 1, protocol)
 # message2.parameters << TWP3::Parameter.new('text', :string)
 # message2.parameters << TWP3::Parameter.new('number_of_letters', :int)
 # 
-# protocol = TWP3::Protocol.new('Echo', 2)
 # protocol.messages << message1
 # protocol.messages << message2
 # 
-# spec = TWP3::Specification.new
 # spec.protocols << protocol
+# 
+# Echo = protocol
+#
+# puts spec.inspect
 
-puts spec.inspect
+TWP3::Connection.setup('www.dcl.hpi.uni-potsdam.de', 80)
 
-# so the parsed result of that should be like the following?
-class Echo
-  def Request(text)
-    [text, text.length]
-  end
+reply = Echo.Request('test')
 
-  def Reply(text, nr_of_letters)
-    nil
-  end
+puts "#{reply.name} = ID #{reply.id}"
+reply.parameters.each do |param|
+  puts "- #{param.type} #{param.name} = #{param.value}"
 end
 
+# so the parsed result of that should be like the following?
+# class Echo
+#   def Request(text)
+#     [text, text.length]
+#   end
+# 
+#   def Reply(text, nr_of_letters)
+#     nil
+#   end
+# end
+
 # old stuff that works
-con = TWP3::Connection.new('www.dcl.hpi.uni-potsdam.de', 80)
-con.protocol = 2
-con.connect
+# con = TWP3::Connection.new('www.dcl.hpi.uni-potsdam.de', 80)
+# con.protocol = 2
+# con.connect
+# 
+# msg = TWP3::Messages::Request.new(con)
+# msg.message = 0
+# msg.parameters << TWP3::Types::ShortString.new('test')
+# msg.submit
+# 
+# response = TWP3::Messages::Response.new(con)
+# response.receive
+# 
+# response.print
+# 
+# con.disconnect
 
-msg = TWP3::Messages::Request.new(con)
-msg.message = 0
-msg.parameters << TWP3::Types::ShortString.new('test')
-msg.submit
-
-response = TWP3::Messages::Response.new(con)
-response.receive
-
-response.print
-
-con.disconnect
+# hint for socket closing:
+#
+# def do_at_exit(str1)
+#   at_exit { print str1 }
+# end
+# at_exit { puts "cruel world" }
+# do_at_exit("goodbye ")
+# exit
