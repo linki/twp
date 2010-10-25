50d70096990740760585ec5ec0f466aaee177645
Martin Linkhorst <m.linkhorst@googlemail.com>
2010-10-25 23:31:01 +0200

    added DSL

--- client.rb
+++ client.rb
@@ -42,9 +42,14 @@ module TWP3
     end
     
     def connect
-      @socket = TCPSocket.new(@host, @port)
-      @socket.write MAGIC_BYTES
-      @socket.write @protocol
+      begin
+        @socket = TCPSocket.new(@host, @port)
+        @socket.write MAGIC_BYTES
+        @socket.write @protocol
+      rescue Exception
+        @socket.close
+        exit(1)
+      end
     end
     
     def disconnect
@@ -60,7 +65,7 @@ module TWP3
     end
   end
   
-  module Message
+  module Messages
     class Request
       attr_reader :parameters
       
@@ -114,19 +119,145 @@ module TWP3
         end
       end
     end
-  end  
+  end
+  
+  # new stuff
+  
+  class Specification
+    attr_reader :protocols
+    
+    def initialize(&block)
+      @protocols = []
+      instance_exec(&block)
+    end
+  end
+  
+  class Protocol
+    attr_reader :messages
+    
+    def initialize(name, id, &block)
+      @name, @id = name, id
+      @messages  = []
+      instance_exec(&block)
+    end
+  end
+  
+  class Message
+    attr_reader :parameters
+    
+    def initialize(name, id, &block)
+      @name, @id = name, id
+      @parameters = []
+      instance_exec(&block)
+    end
+  end
+  
+  class Parameter
+    def initialize(name, type)
+      @name, @type = name, type
+    end
+  end
+  
+  # here's the magic
+  
+  module DSL
+    def self.included(base)
+      # add the following DSL methods to the classes only when we include the DSL module
+      Specification.send :include, SpecificationMethods
+      Protocol.send      :include, ProtocolMethods
+      Message.send       :include, MessageMethods
+    end
+    
+    def specification(&block)
+      Specification.new(&block)
+    end
+    
+    module SpecificationMethods
+      def protocol(name, id, &block)
+        @protocols << Protocol.new(name, id, &block)
+      end
+    end
+    
+    module ProtocolMethods
+      def message(name, id, &block)
+        @messages << Message.new(name, id, &block)
+      end
+    end
+    
+    module MessageMethods
+      def parameter(name, type)
+        @parameters << Parameter.new(name, type)
+      end
+      
+      def string(name)
+        parameter(name, :string)
+      end
+
+      def int(name)
+        parameter(name, :int)
+      end
+    end
+  end
 end
 
+# only to encapsulate
+include TWP3::DSL
+
+# sweet?!
+spec =
+specification do
+  protocol 'Echo', 2 do
+    message 'Request', 0 do
+      string :text
+      # short for parameter :text, :string
+    end
+    message 'Reply', 1 do
+      string :text
+      int :number_of_letters
+    end
+  end
+end
+
+# same as:
+
+# message1 = TWP3::Message.new('Request', 0)
+# message1.parameters << TWP3::Parameter.new('text', :string)
+# 
+# message2 = TWP3::Message.new('Reply', 1)
+# message2.parameters << TWP3::Parameter.new('text', :string)
+# message2.parameters << TWP3::Parameter.new('number_of_letters', :int)
+# 
+# protocol = TWP3::Protocol.new('Echo', 2)
+# protocol.messages << message1
+# protocol.messages << message2
+# 
+# spec = TWP3::Specification.new
+# spec.protocols << protocol
+
+puts spec.inspect
+
+# so the parsed result of that should be like the following?
+class Echo
+  def Request(text)
+    [text, text.length]
+  end
+
+  def Reply(text, nr_of_letters)
+    nil
+  end
+end
+
+# old stuff that works
 con = TWP3::Connection.new('www.dcl.hpi.uni-potsdam.de', 80)
 con.protocol = 2
 con.connect
 
-msg = TWP3::Message::Request.new(con)
+msg = TWP3::Messages::Request.new(con)
 msg.message = 0
 msg.parameters << TWP3::Types::ShortString.new('test')
 msg.submit
 
-response = TWP3::Message::Response.new(con)
+response = TWP3::Messages::Response.new(con)
 response.receive
 
 response.print
